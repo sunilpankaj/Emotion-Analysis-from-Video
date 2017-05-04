1dc1b33de9f3e7472c448338a73b937b4932c7f7
Sunil Pankaj <ee14btech11032@iith.ac.in>
2017-05-04 20:35:54 +0530

    Add files via upload

---
+++ train_original/test.py
@@ -0,0 +1,391 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+# In[76]:
+
+#data_original = pd.read_csv("fer2013.csv")
+#data = data_original.head(100)
+data = pd.read_csv("fer2013.csv")
+
+train_data = data[data.Usage == "Training"]
+pixels_values = train_data.pixels.str.split(" ").tolist()
+
+
+# In[83]:
+
+pixels_values = pd.DataFrame(pixels_values, dtype=int)
+
+
+# In[84]:
+
+images = pixels_values.values
+
+
+# In[85]:
+
+images = images.astype(np.float)
+images = images - images.mean(axis=1).reshape(-1,1)
+
+
+# In[90]:
+
+images = np.multiply(images,100.0/255.0)
+
+
+# In[91]:
+
+each_pixel_mean = images.mean(axis=0)
+
+
+# In[92]:
+
+each_pixel_std = np.std(images, axis=0)
+
+
+# In[93]:
+
+images = np.divide(np.subtract(images,each_pixel_mean), each_pixel_std)
+
+
+# In[94]:
+
+images.shape
+
+
+# In[95]:
+
+image_pixels = images.shape[1]
+image_width = image_height = np.ceil(np.sqrt(image_pixels)).astype(np.uint8)
+
+labels_flat = train_data["emotion"].values.ravel()
+
+
+# In[99]:
+
+labels_count = np.unique(labels_flat).shape[0]
+
+def dense_to_one_hot(labels_dense, num_classes):
+    num_labels = labels_dense.shape[0]
+    index_offset = np.arange(num_labels) * num_classes
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+
+# In[102]:
+
+labels = dense_to_one_hot(labels_flat, labels_count)
+labels = labels.astype(np.uint8)
+
+
+# split data into training & validation
+VALIDATION_SIZE = 1709
+
+
+# In[105]:
+
+validation_images = images[:VALIDATION_SIZE]
+validation_labels = labels[:VALIDATION_SIZE]
+
+train_images = images[VALIDATION_SIZE:]
+train_labels = labels[VALIDATION_SIZE:]
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=1e-4)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+# In[108]:
+
+# convolution
+def conv2d(x, W, padd):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padd)
+
+
+# In[109]:
+
+# pooling
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+# In[110]:
+
+# input & output of NN
+
+# images
+x = tf.placeholder('float', shape=[None, image_pixels])
+# labels
+y_ = tf.placeholder('float', shape=[None, labels_count])
+
+
+# In[111]:
+
+# first convolutional layer 64
+W_conv1 = weight_variable([5, 5, 1, 64])
+b_conv1 = bias_variable([64])
+
+# (27000, 2304) => (27000,48,48,1)
+image = tf.reshape(x, [-1,image_width , image_height,1])
+#print (image.get_shape()) # =>(27000,48,48,1)
+
+
+h_conv1 = tf.nn.relu(conv2d(image, W_conv1, "SAME") + b_conv1)
+#print (h_conv1.get_shape()) # => (27000,48,48,64)
+h_pool1 = max_pool_2x2(h_conv1)
+#print (h_pool1.get_shape()) # => (27000,24,24,1)
+h_norm1 = tf.nn.lrn(h_pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
+
+
+# In[112]:
+
+# second convolutional layer
+W_conv2 = weight_variable([5, 5, 64, 128])
+b_conv2 = bias_variable([128])
+
+h_conv2 = tf.nn.relu(conv2d(h_norm1, W_conv2, "SAME") + b_conv2)
+#print (h_conv2.get_shape()) # => (27000,24,24,128)
+
+h_norm2 = tf.nn.lrn(h_conv2, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
+
+h_pool2 = max_pool_2x2(h_norm2)
+
+
+# In[113]:
+
+# local layer weight initialization
+def local_weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.04)
+    return tf.Variable(initial)
+
+def local_bias_variable(shape):
+    initial = tf.constant(0.0, shape=shape)
+    return tf.Variable(initial)
+
+
+# In[114]:
+
+# densely connected layer local 3
+W_fc1 = local_weight_variable([12 * 12 * 128, 3072])
+b_fc1 = local_bias_variable([3072])
+
+# (27000, 12, 12, 128) => (27000, 12 * 12 * 128)
+h_pool2_flat = tf.reshape(h_pool2, [-1, 12 * 12 * 128])
+
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+#print (h_fc1.get_shape()) # => (27000, 1024)
+
+
+# In[115]:
+
+# densely connected layer local 4
+W_fc2 = local_weight_variable([3072, 1536])
+b_fc2 = local_bias_variable([1536])
+
+# (40000, 7, 7, 64) => (40000, 3136)
+h_fc2_flat = tf.reshape(h_fc1, [-1, 3072])
+
+h_fc2 = tf.nn.relu(tf.matmul(h_fc2_flat, W_fc2) + b_fc2)
+#print (h_fc1.get_shape()) # => (40000, 1024)
+
+
+# In[116]:
+
+# dropout
+keep_prob = tf.placeholder('float')
+h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
+
+
+# In[117]:
+
+# readout layer for deep net
+W_fc3 = weight_variable([1536, labels_count])
+b_fc3 = bias_variable([labels_count])
+
+y = tf.nn.softmax(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
+
+#print (y.get_shape()) # => (40000, 10)
+
+
+# In[118]:
+
+# settings
+LEARNING_RATE = 1e-4
+
+
+# In[119]:
+
+# cost function
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+
+# optimisation function
+train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
+# evaluation
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+
+# In[120]:
+
+# prediction function
+#[0.1, 0.9, 0.2, 0.1, 0.1 0.3, 0.5, 0.1, 0.2, 0.3] => 1
+predict = tf.argmax(y,1)
+
+
+# In[121]:
+
+# set to 3000 iterations
+TRAINING_ITERATIONS = 20
+
+DROPOUT = 0.5
+BATCH_SIZE = 50
+
+
+# In[122]:
+
+epochs_completed = 0
+index_in_epoch = 0
+num_examples = train_images.shape[0]
+
+saver = tf.train.Saver()
+# start TensorFlow session
+sess = tf.InteractiveSession()
+saver.restore(sess, "saved_model/model.ckpt")
+print "Model restored"
+
+# In[124]:
+
+# visualisation variables
+train_accuracies = []
+validation_accuracies = []
+x_range = []
+
+display_step=1
+
+
+
+# ## Results analysis visualizatioin
+
+# In[126]:
+
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import itertools
+
+
+# In[127]:
+
+# check final accuracy on validation set
+if(VALIDATION_SIZE):
+    validation_accuracy = accuracy.eval(session=sess, feed_dict={x: validation_images,
+                                                   y_: validation_labels,
+                                                   keep_prob: 1.0})
+    print('validation_accuracy => %.4f'%validation_accuracy)
+
+
+test_data = data[data.Usage == "PublicTest"]
+
+
+# In[131]:
+
+test_data.head()
+
+
+# In[132]:
+
+print "len test_data:", len(test_data)
+
+
+# In[133]:
+test_pixels_values = test_data.pixels.str.split(" ").tolist()
+
+
+# In[134]:
+
+test_pixels_values = pd.DataFrame(test_pixels_values, dtype=int)
+
+
+# In[135]:
+
+test_images = test_pixels_values.values
+
+
+# In[136]:
+
+test_images = test_images.astype(np.float)
+
+
+# In[137]:
+
+test_images = test_images - test_images.mean(axis=1).reshape(-1,1)
+test_images = np.multiply(test_images,100.0/255.0)
+
+
+# In[138]:
+
+test_images = np.divide(np.subtract(test_images,each_pixel_mean), each_pixel_std)
+
+
+# In[139]:
+
+print('test_images({0[0]},{0[1]})'.format(test_images.shape))
+
+# predict test set
+#predicted_lables = predict.eval(feed_dict={x: test_images, keep_prob: 1.0})
+
+# using batches is more resource efficient
+predicted_lables = np.zeros(test_images.shape[0])
+
+
+# In[140]:
+
+for i in range(0,test_images.shape[0]//BATCH_SIZE):
+    predicted_lables[i*BATCH_SIZE : (i+1)*BATCH_SIZE] = predict.eval(feed_dict={x: test_images[i*BATCH_SIZE : (i+1)*BATCH_SIZE],
+                                                                                keep_prob: 1.0})
+
+
+print('predicted_lables({0})'.format(len(predicted_lables)))
+
+
+# In[141]:
+
+print predicted_lables
+
+
+# In[142]:
+
+test_data.emotion.values
+
+
+# In[143]:
+
+print "accuracy_score: ", accuracy_score(test_data.emotion.values, predicted_lables)
+
+
+# In[144]:
+
+print "confusion_matrix: ", confusion_matrix(test_data.emotion.values, predicted_lables)
+
+
+# In[145]:
+
+cnf_matrix = confusion_matrix(test_data.emotion.values, predicted_lables)
+
+# In[147]:
+
+class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+# In[ ]:
